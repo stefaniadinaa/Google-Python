41f9db91e9c665f8a0addf82763f3e9bbe14fc72
Stefania <stefania.dina@stud.acs.upb.ro>
2021-11-25 21:10:38 +0200

    aplicatie1-terminata

--- proiect/aplicatie1/urls.py
+++ proiect/aplicatie1/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 from aplicatie1 import views
 
 app_name = 'aplicatie1'
 
 urlpatterns = [
-    path('', views.CreateLocationView.as_view(), name = 'adaugare')
-
+    path('', views.CreateLocationView.as_view(), name='adaugare'),
+    path('<int:pk>/update/', views.UpdateLocationView.as_view(), name='modificare'),
+    path('list/', views.ListLocationView.as_view(), name='listare')
 ]
